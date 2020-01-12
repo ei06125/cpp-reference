69c2bc5b9426289d3fac81cb70576a535625ed96
Pedro André Oliveira <ei06125@gcloud.fe.up.pt>
2020-01-12 10:45:41 +0000

    Added CMakeLists and main source file

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/******************************************************************************
+ * @brief Application entry point
+ * - The main function builds the objects necessary for the system,
+ * ... then passes them to the application, which simply uses them.
+ *****************************************************************************/
+int main() {
+    std::cout << "Hello world\n";
+    return 0;
+}
