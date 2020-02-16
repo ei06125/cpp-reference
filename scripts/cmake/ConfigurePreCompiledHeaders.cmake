937b8eb077ae48829ea2b3250d8408fe59af9cf7
Pedro André Oliveira <ei06125@gcloud.fe.up.pt>
2020-02-16 16:18:39 +0000

    Added support for WIN precompiled header files

--- scripts/cmake/ConfigurePreCompiledHeaders.cmake
+++ scripts/cmake/ConfigurePreCompiledHeaders.cmake
@@ -2,15 +2,19 @@ message(STATUS "Executing Main ConfigurePreCompiledHeaders")
 
 if(${CMAKE_VERSION} LESS 16)
 
-  add_library(pch ${ProjectIncludeFolder}/pch.hpp)
-  target_sources(pch PRIVATE ${ProjectSourceFolder}/pch.cpp)
+  add_library(pch ${ProjectSourceFolder}/pch.cpp)
   target_include_directories(pch PUBLIC ${ProjectIncludeFolder})
-  add_custom_command(
-    TARGET pch
-    POST_BUILD
-    COMMAND echo ARGS "Precompiling Headers"
-    COMMAND g++ ${ProjectIncludeFolder}/pch.hpp
-    COMMAND echo ARGS "Precompiling Headers - done")
+  if(MSVC)
+    add_custom_command(
+      TARGET pch
+      POST_BUILD
+      COMMAND cl /c /permissive- /W3  /I ${ProjectIncludeFolder} /Yc"pch.hpp" ${ProjectSourceFolder}/pch.cpp)
+  else()
+    add_custom_command(
+      TARGET pch
+      POST_BUILD
+      COMMAND g++ ${ProjectIncludeFolder}/pch.hpp)
+  endif()
 
   function(target_precompile_headers target scope header)
     add_dependencies(${target} pch)
