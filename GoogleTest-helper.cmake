f633f992d490eb3c616193d7f7db6a8d27b6d095
Pedro André <pedro.andre@fabamaq.com>
2020-09-15 17:55:57 +0100

    WIP - CMAKE_PROJECT_INCLUDE_BEFORE

---
+++ GoogleTest-helper.cmake
@@ -0,0 +1,5 @@
+# .../GoogleTest-helper.cmake
+include(cmake/LogSystem/Logger.cmake)
+LogInfo("Running GoogleTest-helper.cmake")
+cmake_policy(SET CMP0048 NEW)
+set(CMAKE_POLICY_DEFAULT_CMP0048 NEW)
